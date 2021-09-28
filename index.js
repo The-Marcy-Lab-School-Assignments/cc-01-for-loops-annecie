109eed7f58c8a108cbd06be8dbdee94b7a85f05e
annecie <ajbexcelsior@gmail.com>
2021-09-28 14:46:04 +0000

    add completed work

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 //1
 function countToFifty(){
-  
+for(let i = 1; i < 51; i++){
+console.log(i)};
 }
 
 //2
-function countFromOne(){
-  
-}
+function countFromOne(number){
+ for(let i = 1; i <= number; i++){
+console.log(i)}
+ }  
+ 
 
 //3
 function threeAndFiveInFifty(){
-  
+for ()
 }
 
 //4
 function threeAndFive(){
   
 }
-
+countToFifty();
 
